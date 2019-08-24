96615f984ff5ad3ba1eeb17d0eb824677878b12b
Zack Yang <zackyang214@gmail.com>
2019-08-25 02:01:12 +1000

    Change test files pathes

--- examples/lib_example.rs
+++ examples/lib_example.rs
@@ -6,13 +6,13 @@ use rand::Rng;
 
 
 fn main() {
-    let left = mat::Mat::load_png("examples/test_left.png");
-    let right = mat::Mat::load_png("examples/test_right.png");
+    let left = mat::Mat::load_png("examples/tests/test_left.png");
+    let right = mat::Mat::load_png("examples/tests/test_right.png");
     let result = eva_lib::stitch_left_right(left, right);
     result.save_as_png("lib_example.png");
 
-    let top = mat::Mat::load_jpeg("examples/top.jpg");
-    let bottom = mat::Mat::load_jpeg("examples/bottom.jpg");
+    let top = mat::Mat::load_jpeg("examples/tests/top.jpg");
+    let bottom = mat::Mat::load_jpeg("examples/tests/bottom.jpg");
     let result = eva_lib::stitch_top_bottom(top, bottom);
     result.save_as_png("lib_example_top_bottom.png");
 }
